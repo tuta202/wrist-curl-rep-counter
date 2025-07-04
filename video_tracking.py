7017d4590c17b2befc49bfefb041ef2c02ed01bf
tuchan2002 <trananhtu171002@gmail.com>
2025-07-04 15:43:31 +0700

    optimize code p1

--- video_tracking.py
+++ video_tracking.py
@@ -1,51 +1,48 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
 from collections import defaultdict
 import cv2
 import numpy as np
 from ultralytics import YOLO
 
-MOTION_THRESHOLD = 5
-REP_UP_THRESHOLD = -5
-REP_DOWN_THRESHOLD = 5
-
 # Load models
 model_det = YOLO("./runs/detect/train/weights/best.pt")  # custom model to detect weight
-model_pose = YOLO("yolo11n-pose.pt")  # YOLOv8 pose model (you can use s/m/l)
+model_pose = YOLO("yolo11n-pose.pt")  # YOLO11n pose model
 
 # Open video
-video_path = "./test_images/wrist_curl_test_2.mp4"
+video_path = "./test_images/wrist_curl_test.mp4"
 cap = cv2.VideoCapture(video_path)
-
 fps = cap.get(cv2.CAP_PROP_FPS)
-print("FPS:", fps, "COOLDOWN:", int(fps / 2))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-out = cv2.VideoWriter("output_tracking_2.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
+out = cv2.VideoWriter("output_tracking.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
 
+# Constants
+MOTION_THRESHOLD = 10
+REP_UP_THRESHOLD = -5
+REP_DOWN_THRESHOLD = 5
+MAX_TRACK_LENGTH = 20
+
+frame_index = 0
+nose_position = None
 track_history = defaultdict(lambda: [])
-
 # Loop video
 while cap.isOpened():
     success, frame = cap.read()
     if not success:
         break
 
-    frame_height, frame_width = frame.shape[:2]
-
-    # Run pose estimation (YOLO pose)
-    pose_results = model_pose.predict(frame, verbose=False, stream=False)[0]
-    nose_list = []
-
-    for kp in pose_results.keypoints:
-        kps = kp.xy[0].cpu().numpy()  # shape (17, 2)
-        nose = kps[0]  # Nose = keypoint 0
-        nose_list.append((int(nose[0]), int(nose[1])))
-        # Optional: draw nose
-        cv2.circle(frame, (int(nose[0]), int(nose[1])), 5, (255, 255, 0), -1)
-
-    # Run object tracking (YOLO)
+    if frame_index % int(fps / 2) == 0:  # skip running pose estimation. ~0.5 s
+        print('Run pose estimation. Frame: ', frame_index)
+        pose_results = model_pose.predict(frame, verbose=False, stream=False)[0]
+        if len(pose_results.keypoints) > 0:
+            kps = pose_results.keypoints[0].xy[0].cpu().numpy() # pose_results.keypoints[0] -> Only 1 person
+            nose_keypoint = kps[0]  # Nose = keypoint 0
+            nose_position = (int(nose_keypoint[0]), int(nose_keypoint[1])) 
+    if nose_position:
+        cv2.circle(frame, nose_position, 5, (255, 255, 0), -1)
+        
+    # Run object tracking
     result = model_det.track(frame, persist=True, verbose=False, stream=False)[0]
 
     if result.boxes and result.boxes.is_track:
@@ -59,14 +56,14 @@ while cap.isOpened():
             center = (float(x), float(y))
             track.append(center)
             
-            # Initialize rep tracking
+            # ----------Start rep tracking----------
             if "rep_state" not in track_history:
                 track_history["rep_state"] = {}
 
             if track_id not in track_history["rep_state"]:
                 track_history["rep_state"][track_id] = {
                     "count": 0,
-                    "state": "idle",     # can be: idle → up → down : 1rep → up → down : 2reps → up → down : 3reps
+                    "state": "idle",    # can be: idle → up → down : 1rep → up → down : 2reps → up → down : 3reps
                     "last_y": y,
                     "cooldown": 0
                 }
@@ -92,10 +89,11 @@ while cap.isOpened():
                 if delta_y > REP_DOWN_THRESHOLD:  # down again = full rep
                     if rep_info["cooldown"] == 0:
                         rep_info["count"] += 1
-                        rep_info["cooldown"] = int(fps / 2)  # skip next few frames, 30 FPS → 10 frame = ~0.33 s
+                        rep_info["cooldown"] = int(fps / 2)  # skip next few frames. ~0.5 s
                     rep_info["state"] = "down"
+            # ----------End rep tracking----------
             
-            if len(track) > 30:
+            if len(track) > MAX_TRACK_LENGTH:
                 track.pop(0)
 
             # 1. Motion filter
@@ -105,22 +103,16 @@ while cap.isOpened():
 
             # 2. Only accept box if its center is LOWER than nose
             accept = False
-            for nx, ny in nose_list:
-                if y > ny:  # y : center.y box
-                    accept = True
-                    break
-            if not accept:
-                continue
+            nx, ny = nose_position   
+            if y <= ny: # y : ycenter box
+                continue 
 
             # 3. Draw track
             bx1, by1 = int(x - w / 2), int(y - h / 2)
             bx2, by2 = int(x + w / 2), int(y + h / 2)
             cv2.rectangle(frame, (bx1, by1), (bx2, by2), (0, 255, 0), 2)
-            cv2.putText(frame, f"ID: {track_id}", (bx2 - 60, by1 - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-            cv2.putText(frame, f"Reps: {rep_info['count']}", (bx1, by1 - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
-
+            text = f"ID:{track_id} | Rep:{rep_info['count']}"
+            cv2.putText(frame, text, (bx1, by1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
             points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
             cv2.polylines(frame, [points], isClosed=False, color=(0, 255, 0), thickness=6)
 
@@ -130,6 +122,7 @@ while cap.isOpened():
         break
 
     out.write(frame)
+    frame_index += 1
 
 cap.release()
 cv2.destroyAllWindows()
